3a146977276e48813c35a94186c8d4cc09910160
Durgi Rajesh <58934671+durgirajesh@users.noreply.github.com>
2021-07-23 14:42:46 +0530

    Initial Commit
    https://leetcode.com/problems/string-to-integer-atoi/

--- String to Integer (atoi).cpp
+++ String to Integer (atoi).cpp
@@ -2,32 +2,36 @@ class Solution {
 public:
     int myAtoi(string s) {
         
-        bool flag=true;
+        bool flag=true, sign=false, num=false;
         string str;
         int cnt=0;
+        
         for(int i=0;i<s.length();i++){
-            
-            if(isalpha(s[i]) || s[i]=='.')
-                break;
-            
-            while(s[i]==' ')
+            while(s[i]==' '){
                 i++;
-            
+            }
+            if(isalpha(s[i]) || s[i]=='.' || num || sign){
+                break;
+            }
             if(s[i]=='-'){
                 flag=false;
-                cnt++;
+                cnt++; i++;
             }
             else if(s[i]=='+'){
-                cnt++;
+                cnt++; i++;
             }
+            
+            sign=true;
             
             if(cnt>1){
                 return 0;
             }
             
-            if(s[i]>='0' && s[i]<='9'){
+            while(s[i]>='0' && s[i]<='9'){
                 str+=s[i];
-            }
+                i++;
+                num=true;
+            }i--;
         }	
         
         stringstream in;
@@ -38,10 +42,9 @@ public:
         sum=(flag ? sum : -sum);
         
         if(sum > INT_MAX)
-	     return INT_MAX;
+	    return INT_MAX;
 	else if(sum < INT_MIN)
             return INT_MIN;
-        
         else
             return sum;
     }
